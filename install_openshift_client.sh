f2b016ad678ec99086f482f0ecfc594ad1a4db02
Lincoln Dalgado <dalgado.aws@gmail.com>
2025-03-15 18:25:44 -0500

    instructions to install oc

---
+++ install_openshift_client.sh
@@ -0,0 +1,5 @@
+wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest/openshift-client-linux.tar.gz
+tar -xvzf openshift-client-linux.tar.gz
+sudo mv oc /usr/local/bin/
+sudo chmod +x /usr/local/bin/oc
+
